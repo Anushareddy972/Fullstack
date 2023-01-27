97d4da90caca6b56b71ed8c9936ff37451650a32
Anushareddy972 <reddyanu972@gmail.com>
2023-01-27 14:01:29 -0600

    primenumber

---
+++ src/main/java/com/Fullstack/basics/GeneratePrime.java
@@ -0,0 +1,40 @@
+package com.Fullstack.basics;
+
+import java.util.Scanner;
+
+public class GeneratePrime {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the lb ");
+        int lb = sc.nextInt();
+        System.out.println("Enter the ub ");
+        int ub = sc.nextInt();
+        for(int i=lb;i<=ub;i++)
+        {
+            System.out.println(isPrime(i));
+        }
+
+    }
+
+    public static int isPrime(int n)
+    {
+        int count=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+                count++;
+
+        }
+        if(count==2)
+        {
+            System.out.println(n+" "+"is a prime number");
+        }
+        else{
+            System.out.println(n+" "+"is not a prime number");
+        }
+
+        return n;
+    }
+}
+
